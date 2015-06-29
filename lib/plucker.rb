a4fab22743e2e342ab040a2143e36c98d3468383
George Ding <gding@mdsol.com>
2015-06-29 14:01:22 -0400

    user responses?

--- lib/plucker.rb
+++ lib/plucker.rb
@@ -18,9 +18,24 @@ module Plucker
   def process_steps(steps)
   	puts("Thank you. Now enter the heads of your modified step definition(s):") 
   	puts("--This is everything before 'do' and the argument block.--")
+  	steps.push(gets)
+  	process_steps_helper(steps)
+  end
+
+  def process_user_response
   end
 
   def process_steps_helper(steps)
+  	puts("Thank you. Are there more modified step definitions? (y/n)")
+  	user_response = gets
+  	if user_response == 'y' || user_response == 'Y' || user_response == 'yes' || user_response == 'Yes' || user_response == 'YES'
+  		puts("Enter your modified step definition:")
+  		steps.push(gets)
+  		process_steps_helper(steps)
+  	elsif user_response == 'n' || user_response == 'N' || user_response == 'no' || user_response == 'No' || user_response == 'NO'
+  	else
+
+  	end
 
   end
 
