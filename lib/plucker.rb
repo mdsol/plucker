a48782b7ba1bf051aafd64963d37f3ccf01c2e85
George Ding <gding@mdsol.com>
2015-06-29 13:48:15 -0400

    started main sequence

--- lib/plucker.rb
+++ lib/plucker.rb
@@ -1,7 +1,28 @@
 require "to_regexp"
 
 module Plucker
+
   def main_sequence
+
+  	step_definitions = []
+
+  	puts("")
+  	puts("****-Hello and welcome to plucker!-****")
+  	puts("")
+  	puts("To begin please enter the path for your features directory:")
+  	features_dir = gets
+  	process_steps(step_definitions)
+
   end
+
+  def process_steps(steps)
+  	puts("Thank you. Now enter the heads of your modified step definition(s):") 
+  	puts("--This is everything before 'do' and the argument block.--")
+  end
+
+  def process_steps_helper(steps)
+
+  end
+
 end
 
