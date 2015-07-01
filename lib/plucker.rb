4def77ab553d1d973022536466deb117a68aa9ed
George Ding <gding@mdsol.com>
2015-07-01 16:30:56 -0400

    testing

--- lib/plucker.rb
+++ lib/plucker.rb
@@ -2,15 +2,15 @@ require "to_regexp"
 
 module Plucker
 
-  Struct.new("featuple_one", :feature, :steps)
-  Struct.new("featuple_two", :feature, :count)
+  Struct.new("Featuple", :feature, :steps)
+  Struct.new("Countuple", :feature, :count)
 
-  step_definitions = []
-  features_dir = ""
+  $step_definitions = []
+  $features_dir = ""
 
   def main_sequence
     sequence_start
-    if step_definitions.size == 1
+    if $step_definitions.size == 1
       single_step
     else
       mode_picker
@@ -22,15 +22,15 @@ module Plucker
     puts("****-Hello and welcome to plucker!-****")
     puts("")
     puts("To begin please enter the path for your features directory:")
-    features_dir = gets
-    process_steps(step_definitions)
+    $features_dir = gets
+    process_steps($step_definitions)
   end
 
   def single_step
   end
 
   def shortest_sequence
-    results = process_results(main_search_seq(features_dir,step_defintions))
+    results = process_results(main_search_seq($features_dir,step_defintions))
     puts("Done. Here is the smallest sequence of feature files you must run:")
     puts("")
     puts(results)
@@ -58,30 +58,30 @@ module Plucker
   end
 
   def process_steps(steps)
-  	puts("Thank you. Now enter the heads of your modified step definition(s):") 
-  	puts("--This is everything before 'do' and the argument block.--")
-  	steps.push(gets)
-  	process_steps_helper(steps)
+    puts("Thank you. Now enter the heads of your modified step definition(s):") 
+    puts("--This is everything before 'do' and the argument block.--")
+    steps.push(gets)
+    process_steps_helper(steps)
   end
 
   def process_steps_helper(steps)
-  	puts("Are there more modified step definitions? (Y/N)")
-  	user_response = gets
-  	if user_response == 'y' || user_response == 'Y' || user_response == 'yes' || user_response == 'Yes' || user_response == 'YES'
-  		puts("Enter your modified step definition:")
-  		steps.push(gets)
-  		process_steps_helper(steps)
-  	elsif user_response == 'n' || user_response == 'N' || user_response == 'no' || user_response == 'No' || user_response == 'NO'
-  	else
-  		puts("Sorry that is not a valid response please try again.")
-  		puts("")
-  		process_steps_helper(steps)
-  	end
+    puts("Are there more modified step definitions? (Y/N)")
+    user_response = gets
+    if user_response == 'y' || user_response == 'Y' || user_response == 'yes' || user_response == 'Yes' || user_response == 'YES'
+      puts("Enter your modified step definition:")
+      steps.push(gets)
+      process_steps_helper(steps)
+    elsif user_response == 'n' || user_response == 'N' || user_response == 'no' || user_response == 'No' || user_response == 'NO'
+    else
+      puts("Sorry that is not a valid response please try again.")
+      puts("")
+      process_steps_helper(steps)
+    end
   end
 
   def regexp_match(step_definition,step)
-  	string_reg = step_definition[step_definition.index('/^')..step_definition.index('$/')+1]
-  	string_temp_one = step.lstrip.chomp
+    string_reg = step_definition[step_definition.index('/^')..step_definition.index('$/')+1]
+    string_temp_one = step.lstrip.chomp
     string_step = ""
     if string_temp_one.start_with?('Given')
       string_step = string_temp_one[6..string_temp_one.size-1]
@@ -100,20 +100,20 @@ module Plucker
 
   def main_search_seq(features,steps)
     result = []
-  	Dir.chdir(features)
-  	feature_files = Dir.glob('**/**/*.feature')
-  	feature_files.each do |feature_file|
-      curr_featuple = Struct::featuple_one.new(File.absolute_path(feature_file),[])
-  		steps.each do |step|
-  			File.readlines(feature_file).each do |line|
+    Dir.chdir(features)
+    feature_files = Dir.glob('**/**/*.feature')
+    feature_files.each do |feature_file|
+      curr_featuple = Struct::Featuple.new(File.absolute_path(feature_file),[])
+      steps.each do |step|
+        File.readlines(feature_file).each do |line|
           if regexp_match(step,line)
             curr_featuple[:steps].push(step) 
             break
           end
-  			end
-  		end
+        end
+      end
       result.push(featuple) if curr_featuple[:steps].size >= 1
-  	end
+    end
     result
   end
 
@@ -142,4 +142,5 @@ module Plucker
   end
 
 end
+
 
